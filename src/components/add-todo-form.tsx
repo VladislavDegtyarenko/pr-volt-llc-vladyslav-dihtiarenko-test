c00bf4620d98695d8a8e341f0e492e32bb19aeea
VladislavDegtyarenko <vladislavdegtyarenko@gmail.com>
2024-03-13 10:24:56 +0200

    input validation: max 80 chars

--- src/components/add-todo-form.tsx
+++ src/components/add-todo-form.tsx
@@ -1,5 +1,5 @@
 // Core
-import { ChangeEvent, FC, FormEvent, useState } from 'react'
+import { ChangeEvent, FC, FormEvent, useEffect, useMemo, useState } from 'react'
 
 // UI
 import { Button } from './ui/button'
@@ -7,32 +7,60 @@ import { Input } from './ui/input'
 import { useAppDispatch } from '@/hooks/redux-hooks'
 import { todoAdded } from '@/features/todos-slice'
 
+//
+const maxChars = 80
+const maxCharsMessage = `The task name is too long.
+${maxChars} characters max.`
+
 const AddTodoForm: FC = () => {
   const [inputText, setInputText] = useState('')
+  const [inputError, setInputError] = useState('')
   const dispatch = useAppDispatch()
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
     setInputText(event.target.value)
   }
 
+  const memoizedInputError = useMemo(() => {
+    if (inputText.length > maxChars) {
+      return maxCharsMessage
+    }
+    return ''
+  }, [inputText])
+
+  useEffect(() => {
+    setInputError(memoizedInputError)
+  }, [memoizedInputError])
+
   const handleSubmit = (event: FormEvent<HTMLFormElement>): void => {
     event.preventDefault()
 
-    if (inputText.length > 0) {
+    if (inputText.length > 0 && inputText.length <= maxChars) {
       dispatch(todoAdded(inputText))
       setInputText('')
     }
   }
 
+  /* prettier-ignore */
   return (
-    <form className="flex gap-4 pb-4" onSubmit={handleSubmit}>
+    <form className="relative flex gap-4 pb-4" onSubmit={handleSubmit}>
       <Input
         placeholder="Task name"
         className="font-bold"
         value={inputText}
         onChange={handleChange}
       />
-      <Button disabled={inputText.length === 0}>Add Todo</Button>
+      <Button disabled={inputText.length === 0 || inputText.length > maxChars}>
+        Add Todo
+      </Button>
+
+      {inputError && <span
+        className="text-xs absolute bottom-full mb-1 ml-2
+        text-red-600 dark:text-red-400"
+      >
+        {inputError}
+      </span>
+      }
     </form>
   )
 }
